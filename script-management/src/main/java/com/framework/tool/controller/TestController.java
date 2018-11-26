cf2ef82c4bc3dbd6ac000256cc000de8c300f5ba
adole <15607102706@163.com>
2018-11-26 11:08:55 +0800

    初始化

---
+++ script-management/src/main/java/com/framework/tool/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.framework.tool.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @PostMapping("/test")
+    public void test(){
+        System.out.println("test");
+    }
+}
