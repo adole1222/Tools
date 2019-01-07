f842047d0220dacb3110835356509faa6ddbd166
adole <15607102706@163.com>
2019-01-07 17:18:53 +0800

    初始化

--- script-management/src/main/java/com/framework/tool/config/MybatisConfig.java
+++ script-management/src/main/java/com/framework/tool/config/MybatisConfig.java
@@ -2,7 +2,9 @@ package com.framework.tool.config;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
@@ -11,7 +13,9 @@ import javax.sql.DataSource;
 @Configuration("scriptMybatis")
 public class MybatisConfig {
 
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) {
+    @Bean
+    @Qualifier
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("scriptDataSource") DataSource dataSource) {
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
         bean.setDataSource(dataSource);
         try {
---
+++ script-management/src/main/resources/dbScript/t_test01.sql
@@ -0,0 +1,25 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 39.108.222.143
+Source Server Version : 50642
+Source Host           : 39.108.222.143:3306
+Source Database       : testdb
+
+Target Server Type    : MYSQL
+Target Server Version : 50642
+File Encoding         : 65001
+
+Date: 2019-01-06 23:34:11
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for t_test01
+-- ----------------------------
+DROP TABLE IF EXISTS `t_test01`;
+CREATE TABLE `t_test01` (
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
