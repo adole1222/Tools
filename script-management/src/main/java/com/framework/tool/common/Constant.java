b8d3531cae4fa8262521c3b6709f35b7f422340e
adole <15607102706@163.com>
2019-01-22 17:21:44 +0800

    初始化

--- script-management/src/main/java/com/framework/tool/common/Constant.java
+++ script-management/src/main/java/com/framework/tool/common/Constant.java
@@ -1,8 +1,29 @@
 package com.framework.tool.common;
 
 public class Constant {
-    public static final  String TEST_DB_DATASOURCE = "testDataSource";
+    public static final String TEST_DB_DATASOURCE = "testDataSource";
     public static final String TEST_DB = "testdb";
-    public static final  String USER_DB_DATASOURCE = "userDataSource";
+    public static final String USER_DB_DATASOURCE = "userDataSource";
     public static final String USER_DB = "userdb";
+
+    /**
+     * 代办状态
+     */
+    public static final int STATUS_TODO = 0;
+
+    /**
+     * 执行中状态
+     */
+    public static final int STATUS_DOING = 1;
+
+    /**
+     * 成功状态
+     */
+    public static final int STATUS_SUCCESS = 2;
+
+    /**
+     * 失败状态
+     */
+    public static final int STATUS_FAIL = -1;
+
 }
--- script-management/src/main/java/com/framework/tool/service/UtilDataSource.java
+++ script-management/src/main/java/com/framework/tool/service/UtilDataSource.java
@@ -1,7 +1,12 @@
 package com.framework.tool.service;
+
 import com.framework.tool.common.Constant;
+import org.apache.ibatis.jdbc.ScriptRunner;
 
 import javax.sql.DataSource;
+import java.io.File;
+import java.io.FileReader;
+import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,16 +14,28 @@ public class UtilDataSource {
 
     static Map<String, String> dataSourceKeyMap = new HashMap<String, String>() {
         {
-            put(Constant.TEST_DB, Constant.TEST_DB_DATASOURCE );
-            put(Constant.USER_DB, Constant.USER_DB_DATASOURCE );
+            put(Constant.TEST_DB, Constant.TEST_DB_DATASOURCE);
+            put(Constant.USER_DB, Constant.USER_DB_DATASOURCE);
         }
     };
 
     public static DataSource getDataSourceMap(String key, Map<String, DataSource> dataSourceMap) {
         String dataSourceKey = dataSourceKeyMap.get(key);
-        if (!dataSourceMap.containsKey(dataSourceKey)){
+        if (!dataSourceMap.containsKey(dataSourceKey)) {
             return dataSourceMap.get(Constant.TEST_DB_DATASOURCE);
         }
         return dataSourceMap.get(dataSourceKey);
+    }
+
+
+    public static void executeScript(ScriptRunner scriptRunner, DataSource dataSource, File script) {
+        try {
+            scriptRunner.runScript(new FileReader(script));
+            String sql = String.format("UPDATE `testdb`.`t_schema_table` SET `id`=NULL, `name`=NULL, `status`=NULL, `add_time`=NULL, `add_user`=NULL, `update_time`=NULL, `update_user`=NULL WHERE (`id`=0);",script.getName());
+            dataSource.getConnection().prepareStatement(sql);
+        }catch (Exception e){
+
+        }
+
     }
 }
